264d85920e7bddb9298830156ec35e92b801ef24
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-12-22 17:30:57 +0100

    build(user-service): added deps

--- UsersService/build.gradle.kts
+++ UsersService/build.gradle.kts
@@ -12,10 +12,19 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.vertx:vertx-core:4.4.6")
+    implementation("io.vertx:vertx-web:4.4.6")
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.11.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 kotlin {
@@ -23,5 +32,5 @@ kotlin {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("LauncherKt")
 }
