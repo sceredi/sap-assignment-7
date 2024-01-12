7fbe5c480747370029b1fd8bf42b8d25ba3b7ffd
SimoneCeredi <ceredi.simone.iti@gmail.com>
2024-01-12 18:13:47 +0100

    feat(APIGateway): created routes for escooter service

---
+++ APIGateway/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "sap.escooters"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.0"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.slf4j:slf4j-api")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
