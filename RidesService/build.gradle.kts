4607e3b2deb51e50f74aa40f314c7ce38eb52d95
sceredi <ceredi.simone.iti@gmail.com>
2024-05-02 16:13:28 +0200

    chore(Rides): update deps

--- RidesService/build.gradle.kts
+++ RidesService/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.23"
     application
 }
 
@@ -12,6 +12,9 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+    testImplementation("io.mockk:mockk:1.13.10")
     implementation("io.vertx:vertx-core:4.4.6")
     implementation("io.vertx:vertx-web:4.4.6")
     implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.11.0")
@@ -22,7 +25,7 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 application {
