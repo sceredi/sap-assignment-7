9caa5f760f35ac42d65cf370a91fd42622f69a72
sceredi <ceredi.simone.iti@gmail.com>
2024-05-12 16:54:08 +0200

    feat(EScooters): implemented get escooter from db

--- EScooters/internal/adapters/repository/escooters.go
+++ EScooters/internal/adapters/repository/escooters.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/sceredi/sap-assignment-5/escooters-service/internal/core/domain"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // RegisterEScooter creates a new escooter instance on the database
@@ -19,4 +20,17 @@ func (e *DB) RegisterEScooter(id string) (*domain.EScooter, error) {
 	}
 	log.Printf("Registered new escooter with _id %v\n", result.InsertedID)
 	return &escooter, nil
+}
+
+// GetEScooter gets the EScooter with the given id from the database
+func (e *DB) GetEScooter(id string) (*domain.EScooter, error) {
+	log.Printf("Getting escooter with id: %s\n", id)
+	result := e.collection.FindOne(context.TODO(), bson.M{"_id": id})
+	var escooter domain.EScooter
+	if err := result.Decode(&escooter); err != nil {
+		return nil, err
+	}
+	log.Printf("Got escooter with id: %s\n", escooter.Id)
+	return &escooter, nil
+
 }
