8694c43844d249774d07a303282b893be28f6733
sceredi <ceredi.simone.iti@gmail.com>
2024-06-25 17:43:36 +0200

    chore(dashboard): add requests_total metric

--- EScootersManagementSystemDashboard/main.go
+++ EScootersManagementSystemDashboard/main.go
@@ -7,7 +7,30 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// Create a new Prometheus counter
+var (
+	requestsTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "requests_total",
+		Help: "The total number of received requests",
+	})
+)
+
+func init() {
+	// Register the counter with Prometheus
+	prometheus.MustRegister(requestsTotal)
+}
+
+// Middleware to count requests
+func requestCounter(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		requestsTotal.Inc()
+		return next(c)
+	}
+}
 
 func main() {
 	fmt.Println("EScooters Frontend")
@@ -15,6 +38,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
+	e.Use(requestCounter)
 	e.Static("/static", "assets")
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Healthy")
@@ -23,5 +47,7 @@ func main() {
 		os.Exit(1)
 		return c.String(http.StatusOK, "Kill Confirmed")
 	})
-	e.Start(":3000")
+	// Route to expose Prometheus metrics
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+	e.Logger.Fatal(e.Start(":3000"))
 }
